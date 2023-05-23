49e85de4dbae49aa1603433c271938be2ff8e633
sabeur_mohammed_amin <sabeurmohammedamin@gmail.com>
2023-05-23 11:52:24 +0200

    adding vuetify theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   modules: ["@nuxt/devtools"],
   devtools: {
@@ -8,7 +9,7 @@ export default defineNuxtConfig({
   css: [
     '@mdi/font/css/materialdesignicons.min.css',
     ],
-
+  plugins:["vuetify"],
   build: {
     transpile: ['vuetify'],
   },
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,15 +2,23 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-// Styles
 //import "vuetify/styles";
+import "vuetify/styles";
 import "@mdi/font/css/materialdesignicons.css";
+import myTheme from "@/plugins/themes/simpleTheme.js";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
+    treeShake: true,
     ssr: true,
     components,
     directives,
+    theme: {
+      defaultTheme: "myTheme",
+      themes: {
+        myTheme,
+      },
+    },
   });
 
   nuxtApp.vueApp.use(vuetify);
